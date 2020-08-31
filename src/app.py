6f743627182a35bc70a6aab43eeec78949b43e9d
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2020-08-31 00:34:58 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 __author__ = "Cobbin"
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash
+import requests
 from common.database import Database
 from app_constants import ALLOWED_EXTENSIONS, UPLOAD_FOLDER
 from models.blogs.blog import BlogPost
+from models.postImages.postImage import PostImage
+import models.alerts.constants as AlertConstants
+from werkzeug.utils import redirect
 #---------------------------------------------------------------------------------------------
 
 app = Flask(__name__)
@@ -28,18 +32,38 @@ def trending():
     for idx,post in enumerate(all_posts):
         try:
             if post.post_images:
-                post_images = [image.image_data.decode('utf-8') for image in post.post_images] # current_user.profile_image.decode('utf-8')
+                post_images = [PostImage.get_by_id(image_id).image_data.decode('utf-8') for image_id in post.post_images] # current_user.profile_image.decode('utf-8')
                 all_post_images.append(post_images) 
                 post_indexes.append(idx)
         except:
             post_images =[]
-    post_indexes.reverse()
-    post_indexes.reverse()
+    all_posts.reverse()
     return render_template('trendingposts.html', posts = all_posts, post_images=all_post_images, post_indexes=post_indexes, posts_length=len(all_posts), indexes_length=len(post_indexes))
 
 @app.route('/help')
 def help():
     return render_template('help.html')
+
+def send(name, email, content):
+    return requests.post(
+        AlertConstants.URL,
+        auth = ("api", AlertConstants.API_KEY),
+        data = {
+            "from": AlertConstants.FROM,
+            "to": AlertConstants.ADMINS_EMAIL,
+            "subject": "Email from {}, {}.".format(name, email),
+            "text": "{}".format(content)
+        }
+    )
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    send(name, email, content)
+    flash('Message Sent (:')
+    return redirect(url_for('trending'))
 
 
 from models.users.views import user_blueprint
@@ -48,4 +72,3 @@ from models.comments.views import comment_blueprint
 app.register_blueprint(user_blueprint, url_prefix="/users")
 app.register_blueprint(blog_blueprint, url_prefix="/blogs")
 app.register_blueprint(comment_blueprint, url_prefix="/comments")
-
